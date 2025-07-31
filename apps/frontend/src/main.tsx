f7e0fd605e221aa41ba009902035edb73c68076c
ralexhassle <55973997+ralexhassle@users.noreply.github.com>
2025-07-31 16:58:39 +0200

    Use React Query for auth and user list

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import App from './app/app';
 
@@ -8,10 +9,14 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const queryClient = new QueryClient();
+
 root.render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
 );

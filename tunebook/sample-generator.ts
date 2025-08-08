536f7527523fef16a906236a61d52afe4c0edc92
Alexandre Hassler <alexandre.hassler@swizy.fr>
2025-08-08 11:31:22 +0200

    cleanup: remove old tunebook directory
    The tunebook functionality has been properly migrated to:
    - libs/tunebook (shared library)
    - apps/tunebook-playground (interactive application)
    
    This commit removes the old standalone tunebook directory.

--- tunebook/sample-generator.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ts-node
-
-// Import du générateur principal et CLI
-import { main } from './sample-generator-cli';
-
-// Point d'entrée pour npm run generate:samples
-main().catch(console.error);
